b8cbcb418dd063fa18426a19b50ac8ef87eaef3e
Alexandra_Kelle <alexandra_kelle@MacBook-Air-Alexandra.local>
2019-03-24 21:41:23 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
+const rates = {
+    H: 50,
+    Q: 25,
+    D: 10,
+    N: 5,
+    P: 1,
+};
+
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    if (currency > 10000) {
+        return { error: "You are rich, my friend! We don't have so much coins for exchange" };
+    }
+
+    const result = {};
+    Object.entries(rates).forEach(([key, value]) => {
+        const count = Math.floor(currency / value);
+        if (count > 0) {
+            result[key] = count;
+            currency -= value * count;
+        }
+    });
+    return result;
+};
